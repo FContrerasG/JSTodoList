7397a7e950a6745d66f9bf4b130e43514c233eed
FContrerasG <fernandocon7rerasg@gmail.com>
2022-08-23 11:54:57 +0200

    sunbit

---
+++ js/components/add-todo.js
@@ -0,0 +1,23 @@
+ export default class AddTodo {
+    constructor() {
+        this.btn = document.getElementById('add');
+        this.title = document.getElementById('title');
+        this.description = document.getElementById('description');
+
+
+    }
+    onClick(callback) {
+        this.btn.onclick = () => {
+            if (title.value === '' || description.value === '') {
+                // alert.classList.remove('d-none');
+                // alert.innerText = 'No hay titulo o descripcion';
+                // return;
+                console.error('incorrecto')
+            } else {
+                callback(this.title.value, this.description.value)
+            }
+
+        }
+    }
+
+}
---
+++ js/model.js
@@ -0,0 +1,68 @@
+export default class Model {
+    constructor() {
+        this.view = null;
+        this.todos = JSON.parse(localStorage.getItem('todos'));
+        if (!this.todos || this.todos.length < 1){
+            this.todos = [
+                {
+                    id : 0,
+                    title: 'Ponte mamadisimo',
+                    description : 'Al puto gym',
+                    completed: false
+                }
+            ]
+            this.currentId = 1;
+        } else {
+            this.currentId = this.todos[
+                this.todos.length -1
+            ].id + 1;
+        }
+
+
+
+        
+
+    }
+    setView(view) {
+        this.view = view;
+    }
+    save() {
+        localStorage.setItem('todos',JSON.stringify(this.todos) )
+    }
+
+
+    getTodos() {
+        return this.todos;
+    }
+    findId(id) {
+        return this.todos.findIndex((todo) => todo.id === id);
+    }
+    toggleComplete(id) {
+        const index = this.findId(id)
+        const todo = this.todos[index]
+        todo.completed = !todo.completed
+        console.log(this.todos)
+
+    }
+    addTodo(title, description) {
+        const todo = {
+            id: this.currentId++,
+            title,
+            description,
+            completed: false
+        }
+        this.todos.push(todo);
+        console.log(this, this.todos);
+        this.save();
+
+        return { ...todo };
+    }
+
+    removeTodo(id) {
+        const index = this.findId(id)
+        this.todos.splice(index, 1);
+        this.save();
+    }
+
+
+}
